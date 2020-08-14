38df59688ff939ca381d51857b50a6c619536c45
Guilherme Calderaro <guilhermecald96@gmail.com>
2020-08-14 13:34:48 -0300

    deploy heroku

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ const GOOGLE_MAPS_API_KEY = 'AIzaSyBcLwqDOS_mRoglMYXbxoxqyBbs-2kWMB0';
 
 const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://...herokuapp.com';
+  : 'https://cadastroentrega.herokuapp.com';
 
 export default {
   GOOGLE_MAPS_API_KEY,
